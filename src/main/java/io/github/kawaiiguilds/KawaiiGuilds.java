df531691bebc9b0491140c7d44a689b67f5d0473
3nzxhsq <34871342+3nzxhsq@users.noreply.github.com>
2017-12-26 23:39:44 +0100

    register first test listeners

--- src/main/java/io/github/kawaiiguilds/KawaiiGuilds.java
+++ src/main/java/io/github/kawaiiguilds/KawaiiGuilds.java
@@ -1,12 +1,16 @@
 package io.github.kawaiiguilds;
 
 import org.bukkit.plugin.java.JavaPlugin;
+import io.github.kawaiiguilds.listener.player.AsyncPlayerChatListener;
+import io.github.kawaiiguilds.listener.player.PlayerJoinListener;
 
 public final class KawaiiGuilds extends JavaPlugin{
 
     @Override
     public void onEnable(){
-
+         Stream.of(
+                new PlayerJoinListener(), AsyncPlayerChatListener()
+        ).forEach(l -> Bukkit.getPluginManager().registerEvents(l, this));
     }
 
     @Override
