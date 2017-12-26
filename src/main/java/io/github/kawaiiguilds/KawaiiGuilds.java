cc79919394f2af9134fb7a5824ff0110db55bf93
Bartssqu <KawaiiGuilds@gmail.com>
2017-12-27 00:26:42 +0100

    release 0.0.2

--- src/main/java/io/github/kawaiiguilds/KawaiiGuilds.java
+++ src/main/java/io/github/kawaiiguilds/KawaiiGuilds.java
@@ -1,15 +1,18 @@
 package io.github.kawaiiguilds;
 
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 import io.github.kawaiiguilds.listener.player.AsyncPlayerChatListener;
 import io.github.kawaiiguilds.listener.player.PlayerJoinListener;
+
+import java.util.stream.Stream;
 
 public final class KawaiiGuilds extends JavaPlugin{
 
     @Override
     public void onEnable(){
          Stream.of(
-                new PlayerJoinListener(), AsyncPlayerChatListener()
+                new PlayerJoinListener(this), new AsyncPlayerChatListener(this)
         ).forEach(l -> Bukkit.getPluginManager().registerEvents(l, this));
     }
 
