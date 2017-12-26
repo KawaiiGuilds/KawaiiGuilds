4f503b1ac8068000939d2a0f97297db0f1a3dd4f
Bartssqu <KawaiiGuilds@gmail.com>
2017-12-26 23:15:58 +0100

    Initial commit

---
+++ src/main/java/io/github/kawaiiguilds/KawaiiGuilds.java
@@ -0,0 +1,18 @@
+package io.github.kawaiiguilds;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class KawaiiGuilds extends JavaPlugin{
+
+    @Override
+    public void onEnable(){
+
+    }
+
+    @Override
+    public void onDisable(){
+
+    }
+
+
+}
