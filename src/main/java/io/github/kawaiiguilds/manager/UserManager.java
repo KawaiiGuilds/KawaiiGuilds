655c129527fb4f9f86788a0c8f804cd85e5660ad
3nzxhsq <34871342+3nzxhsq@users.noreply.github.com>
2017-12-27 14:32:19 +0100

    Update UserManager.java // code style

--- src/main/java/io/github/kawaiiguilds/manager/UserManager.java
+++ src/main/java/io/github/kawaiiguilds/manager/UserManager.java
@@ -10,22 +10,23 @@ public class UserManager {
 
     private final Map<UUID, User> usersMap = new HashMap<>();
 
-    public User createUser(UUID uuid, String name){
+    public User createUser(UUID uuid, String name) {
         if (!this.usersMap.containsKey(uuid)) {
             this.usersMap.put(uuid, new User(uuid, name));
         }
         return this.getUser(uuid);
     }
 
-    public User getUser(UUID uuid){
+    public User getUser(UUID uuid) {
         return this.usersMap.get(uuid);
     }
-
-    public User getUser(String name){
-        return this.usersMap.values()
-                .stream()
-                .filter(Player -> Player.getName().equalsIgnoreCase(name))
+    
+    public User getUser(String name) {
+        return usersOnline.stream()
+                .filter(user -> user.getName().equalsIgnoreCase(name))
+                .map(Optional::ofNullable)
                 .findFirst()
-                .get();
+                .flatMap(Function.identity())
+                .orElse(null);
     }
 }
