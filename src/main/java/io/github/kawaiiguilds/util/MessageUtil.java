8538f8dec23c61f0b7db59af8ef8a2ffd78ce330
BuildTools <3nzxhsq@asdf.pl>
2017-12-27 21:06:05 +0100

    messageutil fixes

--- src/main/java/io/github/kawaiiguilds/util/MessageUtil.java
+++ src/main/java/io/github/kawaiiguilds/util/MessageUtil.java
@@ -2,11 +2,22 @@ package io.github.kawaiiguilds.util;
 
 import org.bukkit.command.CommandSender;
 
+import java.util.Collection;
 import java.util.List;
 
 public class MessageUtil {
 
     private MessageUtil( ){
+    }
+
+    public static void sendMessage(Collection<? extends CommandSender col, List<String> message) {
+        sendMessage(col, message);
+    }
+
+    public static void sendMessage(Collection<? extends CommandSender col, String message) {
+        for (CommandSender commandSender : col) {
+            sendMessage(col, message);
+        }
     }
 
     public static void sendMessage(CommandSender sender, List<String> message) {
