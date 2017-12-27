7114bdb08263afdedf8e3dcc519b30634681af01
Bartssqu <KawaiiGuilds@gmail.com>
2017-12-27 14:43:14 +0100

    format code

--- src/main/java/io/github/kawaiiguilds/KawaiiGuilds.java
+++ src/main/java/io/github/kawaiiguilds/KawaiiGuilds.java
@@ -2,24 +2,20 @@ package io.github.kawaiiguilds;
 
 import io.github.kawaiiguilds.command.basic.args.CreateArgs;
 import io.github.kawaiiguilds.command.executorbase.CommandExecutorBase;
-import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
-import io.github.kawaiiguilds.listener.player.AsyncPlayerChatListener;
 import io.github.kawaiiguilds.listener.player.PlayerJoinListener;
 
 import java.io.File;
-import java.util.stream.Stream;
 
-public final class KawaiiGuilds extends JavaPlugin {
+public final class KawaiiGuilds extends JavaPlugin{
 
     @Override
     public void onEnable(){
         Messages.init(new File(this.getDataFolder(), "messages.yml"));
         Config.init(new File(this.getDataFolder(), "config.yml"));
 
-        Stream.of(
-                new PlayerJoinListener(), new AsyncPlayerChatListener()
-        ).forEach(l -> this.getServer().getPluginManager().registerEvents(l, this));
+
+        this.getServer().getPluginManager().registerEvents(new PlayerJoinListener(), this);
 
         CommandExecutorBase cmdBase = new CommandExecutorBase("kawaiiguilds.command.basic");
         cmdBase.addSubCommand(new CreateArgs(this));
--- src/main/java/io/github/kawaiiguilds/manager/UserManager.java
+++ src/main/java/io/github/kawaiiguilds/manager/UserManager.java
@@ -10,21 +10,21 @@ import java.util.function.Function;
 
 public class UserManager{
 
-    private static final Map<UUID, User> usersMap = new HashMap<>();
+    private static final Map<UUID, User> onlineUsers= new HashMap<>();
 
     public static void createUser(UUID uuid, String name){
-        if (!usersMap.containsKey(uuid)) {
-            usersMap.put(uuid, new User(uuid, name));
+        if (!onlineUsers.containsKey(uuid)) {
+            onlineUsers.put(uuid, new User(uuid, name));
         }
         getUser(uuid);
     }
 
     public static User getUser(UUID uuid){
-        return usersMap.get(uuid);
+        return onlineUsers.get(uuid);
     }
 
     public User getUser(String name){
-        return usersMap.values().stream()
+        return onlineUsers.values().stream()
                 .filter(user -> user.getName().equalsIgnoreCase(name))
                 .map(Optional::ofNullable)
                 .findFirst()
